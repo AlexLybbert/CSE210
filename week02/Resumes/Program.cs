bf952c5893ff7d3496e3c989b043819516a98479
Alex Lybbert <xanderjpn@gmail.com>
2026-03-10 19:11:05 -0600

    Week 2 exercise

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job()
+        {
+            _company = "3PL Central",
+            _jobTitle = "Software Developer",
+            _startYear = 2013,
+            _endYear = 2014
+        };
+
+        Job job2 = new Job()
+        {
+            _company = "Avii",
+            _jobTitle = "Software Developer",
+            _startYear = 2017,
+            _endYear = 2022
+        };
+
+        Job job3 = new Job()
+        {
+            _company = "Brandt",
+            _jobTitle = "Senior Software Developer",
+            _startYear = 2023,
+            _endYear = 2026
+        };
+
+        Resume resume = new Resume()
+        {
+            _name = "Alex Lybbert",
+            _jobs = new List<Job>
+            {
+                job1,
+                job2,
+                job3
+            }
+        };
+
+        resume.Display();
     }
 }
