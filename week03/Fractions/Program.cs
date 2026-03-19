689e456bff704241f87fa6831aeaff9079772a49
Alex Lybbert <xanderjpn@gmail.com>
2026-03-18 20:27:12 -0600

    Fractions

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction one = new Fraction();
+        Fraction five = new Fraction(5);
+        Fraction threeFourths = new Fraction(3, 4);
+        Fraction oneThird = new Fraction(1, 3);
+
+        Console.WriteLine(one.GetFractionString());
+        Console.WriteLine(one.GetDecimalValue());
+
+        Console.WriteLine(five.GetFractionString());
+        Console.WriteLine(five.GetDecimalValue());
+
+        Console.WriteLine(threeFourths.GetFractionString());
+        Console.WriteLine(threeFourths.GetDecimalValue());
+
+        Console.WriteLine(oneThird.GetFractionString());
+        Console.WriteLine(oneThird.GetDecimalValue());
     }
 }
